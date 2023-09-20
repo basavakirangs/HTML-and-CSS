24e79a87a3293609afe92af700c9c0057ac78578
basavakiran-gs <gsbasavakiran@gmail.com>
2023-09-20 20:50:33 +0530

    Adding few more files of practice

---
+++ JS/BlackJack.js
@@ -0,0 +1,11 @@
+let firstCard = 11
+let secondCard = 10
+let sum = firstCard + secondCard
+
+if (sum < 21) {
+    console.log("Don you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Whooo! You've got BlackJack! 🥳")
+}else (sum > 21){
+    console.log("You're out of the game! 😭")
+} 
